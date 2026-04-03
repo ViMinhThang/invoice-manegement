f5526e64d8729d49f0357195ba68055c942def0c
Phạm Quốc Phương Nam <22130175@st.hcmuaf.edu.vn>
2026-04-03 10:56:09 +0700

    code payments

--- invoince-frontend/src/App.tsx
+++ invoince-frontend/src/App.tsx
@@ -1,18 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-import ItemDetailsForm from './form_checkout/checkout'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import ItemDetailsForm from './form_checkout/checkout';
+import PaymentQueue from './Confirm_Payment/confirm_payment';
+
+const Dashboard = () => <div className="p-8">Dashboard Page</div>;
 
 function App() {
-  const [count, setCount] = useState(0)
-
- return (
-  <>
-  <ItemDetailsForm/>
-  </>
- )
+  return (
+    <Router>
+      <Routes>
+        {/* Route mặc định hướng về trang Payment Queue */}
+        <Route path="/" element={<ItemDetailsForm/>} />
+        
+        <Route path="/payments" element={<PaymentQueue />} />
+        
+        <Route path="/dashboard" element={<Dashboard />} />
+        
+        <Route path="*" element={<div className="p-8">404 - Page Not Found</div>} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
